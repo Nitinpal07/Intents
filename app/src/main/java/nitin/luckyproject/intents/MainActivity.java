5b34edec55d7a16d63c7dc0f1bf93639d212580f
Nitin Pal <nitinluck0123@gmail.com>
2019-06-30 00:52:27 +0530

    Passing implicit intent to Gmail

--- app/src/main/java/nitin/luckyproject/intents/MainActivity.java
+++ app/src/main/java/nitin/luckyproject/intents/MainActivity.java
@@ -3,9 +3,11 @@ package nitin.luckyproject.intents;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,10 +18,23 @@ public class MainActivity extends AppCompatActivity {
 
 
         Button btn =findViewById(R.id.btn1);
+        Button btnexplicit =findViewById(R.id.btnexplicit);
+        final EditText edtxt = findViewById(R.id.edittext1);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(),SecondActivity.class);
+                intent.putExtra(edtxt.toString(),"value for activity two");
+                startActivity(intent);
+
+            }
+        });
+
+
+        btnexplicit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://mail.google.com/"));
                 startActivity(intent);
             }
         });
