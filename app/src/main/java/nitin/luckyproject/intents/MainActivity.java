989aaf4756ebcd15911a868873415bb25e3489c8
Nitin Pal <nitinluck0123@gmail.com>
2019-06-30 14:49:47 +0530

    Passing data from 2nd activity to 1st Activity

--- app/src/main/java/nitin/luckyproject/intents/MainActivity.java
+++ app/src/main/java/nitin/luckyproject/intents/MainActivity.java
@@ -1,5 +1,6 @@
 package nitin.luckyproject.intents;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,9 +9,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+public static final int REQUEST_CODE = 111;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(getApplicationContext(),SecondActivity.class);
                 EditText edtxt = findViewById(R.id.edittext1);
                 intent.putExtra("key1",edtxt.getText().toString());
-                startActivity(intent);
+                startActivityForResult(intent,REQUEST_CODE);
 
             }
         });
@@ -39,5 +42,17 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
+            if (data.hasExtra("returnKey1")) {
+                Toast.makeText(this, data.getExtras().getString("returnKey1"),
+                        Toast.LENGTH_SHORT).show();
+                TextView txtview = findViewById(R.id.text1);
+                txtview.setText(data.getExtras().getString("returnKey1"));
+            }
+        }
     }
 }
--- app/src/main/java/nitin/luckyproject/intents/SecondActivity.java
+++ app/src/main/java/nitin/luckyproject/intents/SecondActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
@@ -14,11 +17,31 @@ public class SecondActivity extends AppCompatActivity {
         setContentView(R.layout.activity_second);
         Bundle extras = getIntent().getExtras();
         TextView txt =findViewById(R.id.text2);
+        Button backbtn =findViewById(R.id.btn2);
 
         if (extras != null) {
             String editText = extras.getString("key1");
             txt.setText(editText);
         }
 
+        backbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        
+    }
+
+    @Override
+    public void finish() {
+        // Prepare data intent
+        Intent data = new Intent();
+        EditText editText2 = findViewById(R.id.edittext2);
+        data.putExtra("returnKey1", editText2.getText().toString());
+        // Activity finished ok, return the data
+        setResult(RESULT_OK, data);
+        super.finish();
     }
 }
